583d76b6fe79aa03c23afd13df4740187283f2f7
Pawan <paletipawankumar@gmail.com>
2022-09-21 00:39:08 +0530

    Adding friend function

--- basics/FriendFun.cpp
+++ basics/FriendFun.cpp
@@ -12,11 +12,19 @@ public:
         b = c2;
     }
 
+    friend complex sumComplex(complex d1 , complex d2);
+
     void getNumber()
     {
-        cout << "the addition of " << a << " + " << b << "i" endl;
+        cout << "the addition of " << a << " + " << b << "i" << endl;
     }
 };
+
+complex sumComplex ( complex o1 , complex o2){
+    complex o3;
+    o3.setNumber((o1.a + o2.a) , (o1.b + o2.b));
+    return o3;
+}
 
 int main()
 {
@@ -26,5 +34,9 @@ int main()
 
     o2.setNumber(5, 8);
     o2.getNumber();
+
+    sum = sumComplex(o1,o2);
+    sum.getNumber();
+
     return 0;
 }
---
+++ basics/friend1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+    int a , b;
+
+    public :
+    void setFun(int n1 , int n2){
+        a = n1 ;
+        b = n2;
+    }
+
+    void printVal(){
+        cout<<"here is your entered number " << a << " + " << b << " i" << endl;
+    
+    }
+
+    friend complex sumFun(complex d1 , complex d2);
+};
+
+
+complex sumFun(complex o1 , complex o2){
+    complex o3;
+    o3.setFun((o1.a + o2.a) , (o1.b + o2.b));
+    return o3;
+}
+
+int main(){
+    complex o1 , o2 , sum;
+    o1.setFun(4,5);
+    o1.printVal();
+
+    o2.setFun(6,8);
+    o2.printVal();
+
+    sum = sumFun(o1,o2);
+    sum.printVal();
+
+    return 0;
+}
+
+
+// properties of friend function 
+/*
+ 1. not in the scope of class 
+ 2. usually contains objects as argument 
+ 3. can be delclared in public and private of the class 
+ 4.  
+
+*/
